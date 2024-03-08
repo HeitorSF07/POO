46df1b6f75d05e03a725a6989caff66236d73535
Heitor de Sousa Freitas <heitorsf2007@gmail.com>
2024-03-08 08:05:24 -0300

    Update Balancotrimestral.java

--- Balancotrimestral.java
+++ Balancotrimestral.java
@@ -4,6 +4,8 @@ class Balancotrimestral{
     int gastosFevereiro = 23000;
     int gastosMarco = 17000;
     int gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco;
+    int mediaMensal = (gastosJaneiro + gastosFevereiro + gastosMarco)/3;
     System.out.println(gastosTrimestre);
+    System.out.printf("Valor da média="+mediaMensal);
   }
 }
