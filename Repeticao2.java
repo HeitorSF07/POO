e85a9e87ccc4d0eb326031e35fa12838be06cbab
Heitor de Sousa Freitas <heitorsf2007@gmail.com>
2024-03-10 16:51:21 -0300

    Create Repeticao2.java

---
+++ Repeticao2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Repeticao2{
+    public static void main(String[] args){
+    int result=100;
+      for(int i=101; i<1001; i++){
+        result = result+i;
+      }
+    System.out.println(result);
+    }
+}
